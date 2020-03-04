5ae19e30d9fa369a5418bc11fde714bfe3e855d5
john <john@spinningplates.org>
2020-03-04 14:17:03 -0800

    knexfile setup for testing env

--- knexfile.js
+++ knexfile.js
@@ -24,21 +24,26 @@ module.exports = {
     }
   },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      }
+    }
+  },
 
   production: {
     client: 'pg',
